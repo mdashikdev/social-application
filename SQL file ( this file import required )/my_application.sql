adfff80bf1a8cd2c0592eecf8fe1587fef7206e7
wpexpartashik <fashik1375@gmail.com>
2022-08-11 20:04:03 +0600

    Sql file adder (by the way this site is not prodessional. i created it for my practice)

---
+++ SQL file ( this file import required )/my_application.sql
@@ -0,0 +1,334 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 11, 2022 at 04:01 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `my_application`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment`
+--
+
+CREATE TABLE `comment` (
+  `comment_id` int(11) NOT NULL,
+  `comment_parent_id` varchar(255) NOT NULL,
+  `comment_usr_id` varchar(255) NOT NULL,
+  `comment_post_id` varchar(255) NOT NULL,
+  `comment_text` varchar(255) NOT NULL,
+  `comment_time` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `follow`
+--
+
+CREATE TABLE `follow` (
+  `follow_id` int(11) NOT NULL,
+  `sender_id` varchar(255) NOT NULL,
+  `reciever_id` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `frd`
+--
+
+CREATE TABLE `frd` (
+  `frd_id` int(255) NOT NULL,
+  `sent_rqst_usr_id` varchar(255) NOT NULL,
+  `receive_rqst_usr_id` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `frd_rqst_time` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `group_invite`
+--
+
+CREATE TABLE `group_invite` (
+  `group_invite_id` int(11) NOT NULL,
+  `invited_group_id` varchar(255) NOT NULL,
+  `invited_usr_id` varchar(255) NOT NULL,
+  `disabled` int(2) NOT NULL,
+  `inviter` varchar(255) NOT NULL,
+  `group_invite_time` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `group_members`
+--
+
+CREATE TABLE `group_members` (
+  `members_id` int(11) NOT NULL,
+  `member_user_id` varchar(255) NOT NULL,
+  `group_id` varchar(255) NOT NULL,
+  `role` varchar(10) NOT NULL,
+  `creator` varchar(255) NOT NULL,
+  `disabled` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `group_requests`
+--
+
+CREATE TABLE `group_requests` (
+  `group_request_id` int(11) NOT NULL,
+  `group_Id` varchar(255) NOT NULL,
+  `userId` varchar(255) NOT NULL,
+  `disabled` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `likes`
+--
+
+CREATE TABLE `likes` (
+  `like_id` int(11) NOT NULL,
+  `liked_user_id` varchar(255) NOT NULL,
+  `liked_post_id` varchar(255) NOT NULL,
+  `like_status` varchar(10) NOT NULL,
+  `liked_time` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `msgs`
+--
+
+CREATE TABLE `msgs` (
+  `msg_id` int(255) NOT NULL,
+  `receive_user_id` varchar(255) NOT NULL,
+  `sent_user_id` varchar(255) NOT NULL,
+  `msg` text NOT NULL,
+  `msg_sent_time` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notifications`
+--
+
+CREATE TABLE `notifications` (
+  `notification_id` int(11) NOT NULL,
+  `notification_usr_id` varchar(255) NOT NULL,
+  `activity` varchar(255) NOT NULL,
+  `notification_content_id` varchar(255) NOT NULL,
+  `content_owner` varchar(255) NOT NULL,
+  `content_type` varchar(255) NOT NULL,
+  `notification_time` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `post_id` int(255) NOT NULL,
+  `post_user_id` varchar(255) NOT NULL,
+  `post_content` varchar(255) NOT NULL,
+  `post_image` varchar(255) NOT NULL,
+  `post_code` varchar(255) NOT NULL,
+  `post_owner` varchar(255) NOT NULL DEFAULT '0',
+  `post_user_status` varchar(255) NOT NULL,
+  `post_time` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `unique_id` varchar(255) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `owner` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `pass` varchar(255) NOT NULL,
+  `profile` varchar(255) NOT NULL DEFAULT 'default.png',
+  `cover_photo` varchar(255) NOT NULL DEFAULT 'default_cover.jpg',
+  `actv_status` varchar(255) NOT NULL DEFAULT 'Offline',
+  `address` varchar(255) NOT NULL DEFAULT 'Bangladesh',
+  `signuptime` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6),
+  `user_type` varchar(5) NOT NULL DEFAULT 'user',
+  `group_type` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`comment_id`);
+
+--
+-- Indexes for table `follow`
+--
+ALTER TABLE `follow`
+  ADD PRIMARY KEY (`follow_id`);
+
+--
+-- Indexes for table `frd`
+--
+ALTER TABLE `frd`
+  ADD PRIMARY KEY (`frd_id`);
+
+--
+-- Indexes for table `group_invite`
+--
+ALTER TABLE `group_invite`
+  ADD PRIMARY KEY (`group_invite_id`);
+
+--
+-- Indexes for table `group_members`
+--
+ALTER TABLE `group_members`
+  ADD PRIMARY KEY (`members_id`);
+
+--
+-- Indexes for table `group_requests`
+--
+ALTER TABLE `group_requests`
+  ADD PRIMARY KEY (`group_request_id`);
+
+--
+-- Indexes for table `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`like_id`);
+
+--
+-- Indexes for table `msgs`
+--
+ALTER TABLE `msgs`
+  ADD PRIMARY KEY (`msg_id`);
+
+--
+-- Indexes for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD PRIMARY KEY (`notification_id`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`post_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT for table `follow`
+--
+ALTER TABLE `follow`
+  MODIFY `follow_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+
+--
+-- AUTO_INCREMENT for table `frd`
+--
+ALTER TABLE `frd`
+  MODIFY `frd_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=113;
+
+--
+-- AUTO_INCREMENT for table `group_invite`
+--
+ALTER TABLE `group_invite`
+  MODIFY `group_invite_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `group_members`
+--
+ALTER TABLE `group_members`
+  MODIFY `members_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT for table `group_requests`
+--
+ALTER TABLE `group_requests`
+  MODIFY `group_request_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
+
+--
+-- AUTO_INCREMENT for table `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `like_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
+
+--
+-- AUTO_INCREMENT for table `msgs`
+--
+ALTER TABLE `msgs`
+  MODIFY `msg_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=301;
+
+--
+-- AUTO_INCREMENT for table `notifications`
+--
+ALTER TABLE `notifications`
+  MODIFY `notification_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=128;
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `post_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=196;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=87;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
